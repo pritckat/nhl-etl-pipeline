1d0996b2fbea4880e6247b0538667628d4ff2a73
Katrina Pritchard <42280195+pritckat@users.noreply.github.com>
2023-02-06 15:20:31 -0500

    create select with team list

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,12 @@
+const request = require('superagent');
+
 module.exports = {
-    getIndex: (request, response)=>{
-        response.render('index.ejs')
+  getIndex: async (req, res) => {
+    try {
+      const teams = await request.get('https://statsapi.web.nhl.com/api/v1/teams');
+      res.render('index.ejs', {teams: teams.body.teams});
+    } catch (err) {
+      console.error(err);
     }
+  },
 }
