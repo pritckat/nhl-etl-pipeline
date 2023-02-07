7e3946e7e6792de7c8b4da39613a0fda62278cc2
Katrina Pritchard <42280195+pritckat@users.noreply.github.com>
2023-02-07 13:04:33 -0500

    rename server file server

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 app.set('view engine', 'ejs')
