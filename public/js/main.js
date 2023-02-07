cce457b51bd7d6e23f0d62c9c9177c4159dca4c5
Katrina Pritchard <42280195+pritckat@users.noreply.github.com>
2023-02-07 14:28:43 -0500

    get year select options with team select

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,27 @@
 const teamSelect = document.querySelector('#teamName');
+const teamYearSelect = document.querySelector('#teamYear');
 
 const teamSelectChange = () => {
-    const value = teamSelect.value.split('.');
-    const id = value[0];
-    const year = Number(value[1]);
-    console.log('change', id, year);
+  teamYearSelect.disabled = true;
+  document.querySelectorAll('#teamYear option').forEach(option => option.remove())
+
+  const value = teamSelect.value.split('.');
+  const year = Number(value[1]);
+  const currentYear = new Date().getFullYear();
+  const placeholder = document.createElement('option');
+  placeholder.hidden = true;
+  placeholder.disabled = true;
+  placeholder.selected = true;
+  placeholder.value = null;
+  placeholder.innerHTML = "Select a Year";
+  teamYearSelect.appendChild(placeholder);
+  for (var i = year; i<=currentYear; i++){
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = i;
+    teamYearSelect.appendChild(opt);
+  }
+  teamYearSelect.disabled = false;
 }
 
 teamSelect.addEventListener('change', teamSelectChange)
