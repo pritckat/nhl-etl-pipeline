b14da6a85afa5cdf49e48183591b8f41f4fe91e6
Katrina Pritchard <42280195+pritckat@users.noreply.github.com>
2023-02-08 11:03:51 -0500

    write team data to csv and download

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,7 @@
 const api_util = require('../lib/api_util');
+const csv_util = require('../lib/csv_util');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
   getIndex: async (req, res) => {
@@ -7,7 +10,27 @@ module.exports = {
   },
 
   submit: async (req, res) => {
-    api_util.getTeamInfo(req.body.teamId.split('.')[0], req.body.teamYear);
-    res.redirect('/');
+    api_util.getTeamInfo(req.body.teamId.split('.')[0], req.body.teamYear, (err, info) => {
+      if (err) {
+        console.log('err', err)
+      } else {
+        csv_util.writeTeamCsv(info, (err, filename) => {
+          if (err) {
+            console.log('err', err);
+          } else {
+            res.set({
+              'Location': '/'
+            });
+            res.download(path.resolve(filename), (err) => {
+              if (err) {
+                console.log(err);
+              } else {
+                fs.unlinkSync(path.resolve(filename))
+              }
+            });
+          }
+        })
+      }
+    });
   }
 }
